399b52e0b7ced1aecbfcd88f10d9455ec98d0c45
CB <cristian.barragan@gmail.com>
2025-11-14 15:12:44 +1300

    adding conditional to skip entities that do not have link as a Parent

--- src/CoffeeBeanery/GraphQL/Helper/SqlHelper.cs
+++ src/CoffeeBeanery/GraphQL/Helper/SqlHelper.cs
@@ -120,8 +120,15 @@ public static class SqlHelper
                 whereCurrentClause += $" {whereParentClause} {whereCurrentValue.Replace("~", processingTree.Name)}";
             }
 
-            sqlUpsert += GenerateUpsert(processingTree, trees, sqlUpsertStatementNodes, whereCurrentClause);
-
+            var upsertingEntity = sqlUpsertStatementNodes.FirstOrDefault(s =>
+                s.Key.Split('~')[0].Matches(processingTree.Name) && !s.Value.JoinKeys
+                    .Any(a => a.To.Split('~')[0].Matches(processingTree.ParentName)));
+            
+            if (upsertingEntity.Value != null && upsertingEntity.Value.LinkKeys.Count > 0)
+            {
+                sqlUpsert += GenerateUpsert(processingTree, trees, sqlUpsertStatementNodes, whereCurrentClause);    
+            }
+            
             if ((entityNames.Any(e => processingTree.Children.Any(c => c.Name.Matches(e))) ||
                  entityNames.Any(e => e.Matches(processingTree.ParentName))) &&
                 entityNames.Any(e => e.Matches(processingTree.Name)))
