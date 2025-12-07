54f8bdcd2a008f19ad90ca6101ecd46b7d39d030
CB <cristian.barragan@gmail.com>
2025-12-08 05:04:19 +1300

    updating src folder

--- src/CoffeeBeanery/GraphQL/Model/EntityTreeMap.cs
+++ src/CoffeeBeanery/GraphQL/Model/EntityTreeMap.cs
@@ -15,8 +15,12 @@ public interface IEntityTreeMap<T, M> where T : class where M : class
     public NodeTree NodeTree { get; set; }
 
     public Dictionary<string, NodeTree> DictionaryTree { get; set; }
-
-    public Dictionary<string, SqlNode> LinkDictionaryTree { get; set; }
+    
+    public Dictionary<string, SqlNode> LinkDictionaryTreeNode { get; set; }
+    
+    public Dictionary<string, SqlNode> LinkDictionaryTreeEdge { get; set; }
+    
+    public Dictionary<string, SqlNode> LinkDictionaryTreeMutation { get; set; }
 }
 
 public class EntityTreeMap<T, M> : IEntityTreeMap<T, M>
@@ -37,6 +41,12 @@ public class EntityTreeMap<T, M> : IEntityTreeMap<T, M>
     public Dictionary<string, NodeTree> DictionaryTree { get; set; } =
         new Dictionary<string, NodeTree>(StringComparer.OrdinalIgnoreCase);
     
-    public Dictionary<string, SqlNode> LinkDictionaryTree { get; set; } =
+    public Dictionary<string, SqlNode> LinkDictionaryTreeNode { get; set; } =
+        new Dictionary<string, SqlNode>(StringComparer.OrdinalIgnoreCase);
+    
+    public Dictionary<string, SqlNode> LinkDictionaryTreeEdge { get; set; } =
+        new Dictionary<string, SqlNode>(StringComparer.OrdinalIgnoreCase);
+    
+    public Dictionary<string, SqlNode> LinkDictionaryTreeMutation { get; set; } =
         new Dictionary<string, SqlNode>(StringComparer.OrdinalIgnoreCase);
 }
